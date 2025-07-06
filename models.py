9b547198bf534e127804fd1bd67978fba44233e7
LE TRONG MAI <buncha453@gmail.com>
2025-07-06 19:17:14 +0700

    ga dong tao

--- models.py
+++ models.py
@@ -5,32 +5,46 @@ from datetime import date, datetime
 class SinhVien(BaseModel):
     IDSINHVIEN: str
     HOVATEN: str
-    LOP: str 
+    LOP: str
     NGANHHOC: str
     NGAYSINH: Optional[date]
+
+    class Config:
+        from_attributes = True
 
 class HocKy(BaseModel):
     IDHOCKY: str
     TENHK: str
     NAMHOC: int
 
+    class Config:
+        from_attributes = True
+
 class TieuChiDanhGia(BaseModel):
     IDTIEUCHI: str
     TENTIEUCHI: str
     DIEMTOIDA: int
 
+    class Config:
+        from_attributes = True
+
 class DanhGia(BaseModel):
     IDPHIEU: str
     IDTIEUCHI: str
-    SVDANHGIA: str
-    CANBODANHGIA: str
-    CVHTXACNHANL: str
+    SVDANHGIA: int
+    CANBODANHGIA: int
+    CVHTXACNHAN: int
+
+    class Config:
+        from_attributes = True
 
 class PhieuDiemRenLuyen(BaseModel):
     IDPHIEU: str
     IDSINHVIEN: str
     IDHOCKY: str
-    TONGDIEM: str
-    XEPLOAI:str
+    TONGDIEM: int
+    XEPLOAI: str
     GHICHU: Optional[str]
 
+    class Config:
+        from_attributes = True
